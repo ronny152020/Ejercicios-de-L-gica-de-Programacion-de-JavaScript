a0abb70fa8cb68787460f0f53e0aa8f5b84c7e4a
ronny152020 <85712824+ronny152020@users.noreply.github.com>
2022-01-16 01:02:35 -0500

    Add files via upload

---
+++ Tarea logica de programacion - JavaScript/ejerciciosbasicos.js
@@ -0,0 +1,10 @@
+function contarcadenas(cadenar=""){
+    if(!cadena){
+        console.warn("No haz ingresado ninguna cadena");
+    }else{
+        console.info("la cadena$(cadena)tiene $(cadena.length)caracteres");
+    }
+
+}
+contarcadenas();
+contarcadenas("Hola andres")
